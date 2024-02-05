1a727d1686f650df0e74b5e6e833dfba217d6013
kwchan19 <kevin990099@gmail.com>
2024-02-05 10:47:55 -0500

    Test

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const app = express()
+const port = 3000;
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.send('PDF Generator')
+})
+
+app.post('/pdf', (req, res) => {
+    const data = (req.body.htmlString);
+
+    var htmlObject = document.createElement('div');
+    htmlObject.innerHTML = data;
+    //htmlObject.getElementById("myDiv").style.marginTop = something;
+
+    var doc = new jsPDF();          
+    var elementHandler = {
+      '#ignorePDF': function (element, renderer) {
+        return true;
+      }
+    };
+
+    var elementHTML = htmlObject.querySelector("#myDiv");
+    var tempValue;
+
+    doc.html(elementHTML, {
+        callback: function(doc) {
+            // Save the PDF
+            tempValue = doc.save('sample-document.pdf');
+        },
+        x: 15,
+        y: 15,
+        width: 170, //target width in the PDF document
+        windowWidth: 650 //window width in CSS pixels
+    });
+
+
+    res.send(tempValue)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
